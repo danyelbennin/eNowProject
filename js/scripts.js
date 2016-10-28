7793c5cff9dbf95e75fe0c44e3853caebb570337
danyelbennin <danyelbennin@yahoo.com>
2016-10-28 09:38:47 +0000

    Completed UI for eNow

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+
+jQuery(document).ready(function() {
+	
+    
+    /*
+        Login form validation
+    */
+    $('.login-form input[type="text"], .login-form input[type="password"]').on('focus', function() {
+    	$(this).removeClass('input-error');
+    });
+    
+    $('.login-form').on('submit', function(e) {
+    	
+    	$(this).find('input[type="text"], input[type="password"]').each(function(){
+    		if( $(this).val() == "" ) {
+    			e.preventDefault();
+    			$(this).addClass('input-error');
+    		}
+    		else {
+    			$(this).removeClass('input-error');
+    		}
+    	});
+    	
+    });
+    
+    /*
+        Registration form validation
+    */
+    $('.registration-form input[type="text"]').on('focus', function() {
+    	$(this).removeClass('input-error');
+    });
+    
+    $('.registration-form').on('submit', function(e) {
+    	
+    	$(this).find('input[type="text"]').each(function(){
+    		if( $(this).val() == "" ) {
+    			e.preventDefault();
+    			$(this).addClass('input-error');
+    		}
+    		else {
+    			$(this).removeClass('input-error');
+    		}
+    	});
+    	
+    });
+    
+    
+});
